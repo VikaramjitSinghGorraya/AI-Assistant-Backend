82c2c20ede78f4271c03e6caec3c822c9925fde5
Vikaramjit Goraya <vikaramjitsinghgorraya@gmail.com>
2025-06-29 10:13:29 -0400

    Added env for Frontend URL

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || ["http://localhost:3000"],
+    origin: process.env.FRONTEND_URL || ["http://localhost:3000"],
     credentials: true,
   })
 );
