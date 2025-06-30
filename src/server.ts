9abe9617e91ef55284edd04b34851db0ec1bfa1f
Vikaramjit Goraya <vikaramjitsinghgorraya@gmail.com>
2025-06-30 08:24:02 -0400

    Changed src in Vercel and CORS in server file

--- src/server.ts
+++ src/server.ts
@@ -13,14 +13,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.options(
-  "/{*any}",
+app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   })
 );
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
